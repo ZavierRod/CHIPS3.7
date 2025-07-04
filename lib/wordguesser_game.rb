41c204ccd5984db2b16b6cb175aaeb6a9848a1b4
Zavier Rodrigues <zavier_rod@berkeley.edu>
2025-07-04 03:44:24 +0000

    Finished with part 1

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -6,8 +6,70 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  attr_accessor :word, :guesses, :wrong_guesses
+
+
+  def check_win_or_lose
+    if @guesses.length == @word.length
+      return :win
+    end
+    if @wrong_guesses.length > 6
+      return :lose
+    else
+      return :play
+    end
   end
 
+  def word_with_guesses
+    # create copy of word but replace with dashes
+    answer = '-' * word.length
+    # Go through that, each time 
+    @word.chars.each_with_index do |char, i|
+      @guesses.chars.each do |guess|
+        if guess == char
+          answer[i] = char
+        end
+      end
+    end
+    return answer
+  end
+
+
+
+  def guess(user_guess)
+    # Raise error if not a valid character
+    if user_guess.nil? 
+      raise ArgumentError, "Guess can't be nil"
+    end
+    if user_guess == '' 
+      raise ArgumentError, "Guess can't be nothing"
+    end
+    if !user_guess.match?(/\A\p{Alpha}\z/) 
+      raise ArgumentError, "Guess has to be an alphabetic letter"
+    end
+
+    already_guessed = false
+    user_guess = user_guess.downcase
+    user_guess.chars.each do |char|
+      if !@guesses.include?(char) && !@wrong_guesses.include?(char)# If it hasn't been guessed before
+        if @word.include?(char) # If the character is in the word
+          @guesses << char # Add it to the correct guesses
+        else # If the character is not in the word
+          if !char.match?(/\A\p{Alpha}\z/) # Raise error if not a valid character
+            raise ArgumentError, "Guess has to be an alphabetic letter"
+          else
+            @wrong_guesses << char# Add it to the wrong guesses
+          end
+        end
+      else # If it has been seen before
+        already_guessed = true 
+      end
+    end
+    return !already_guessed # Return true if it hasn't been seen, false if it has been seen
+  end
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
